b674dd457e8ef92166da55866d82b8706ff1cc64
SrLordGrim <127856317+SrLordGrim@users.noreply.github.com>
2024-04-19 05:19:56 -0500

    Update header.js

--- header.js
+++ header.js
@@ -1,4 +1,13 @@
 $(document).ready(function(){
+  // Función para verificar si la barra lateral está activa al cargar la página
+  function checkSidebarStatus() {
+    if ($('.side-bar').hasClass('active')) {
+      $('.menu-btn').css("visibility", "hidden");
+    } else {
+      $('.menu-btn').css("visibility", "visible");
+    }
+  }
+
   // jQuery para expandir y contraer la barra lateral
   $('.menu-btn').click(function(){
     $('.side-bar').addClass('active');
@@ -11,6 +20,9 @@ $(document).ready(function(){
     // Restaurar la visibilidad del botón de menú
     $('.menu-btn').css("visibility", "visible");
   });
+
+  // Llamar a la función al cargar la página para establecer la visibilidad del botón de menú
+  checkSidebarStatus();
 
   // Cargar el header desde header.html
   $('#header-container').load('/header.html');
